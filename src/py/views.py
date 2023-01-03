2228ff7f3a21f2d5ebd48a8132e219263844c08d
xarxziux <14932450+xarxziux@users.noreply.github.com>
2023-01-03 01:18:03 +0000

    Add original Python and Go port of Longturn warcalc function

---
+++ src/py/views.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+#from django import forms
+#from django.http import HttpResponse, HttpResponseRedirect, Http404
+#from django.shortcuts import render
+#from django.template import Context, TemplateDoesNotExist
+#from django.template.loader import get_template
+#from longturn.warcalc.forms import *
+#from django.conf import settings
+import datetime
+from math import pow, floor
+#from tempfile import NamedTemporaryFile
+#from os import system
+
+def warcalc(request):
+        def remhp(atthp, defhp, attfp, deffp, attpo, defpo):
+                p = float(attpo / float(defpo + attpo))
+                k = int(floor(defhp / attfp))
+                l = int(floor(atthp / deffp))
+
+                def newt(n, k):
+                        l1 = 1
+                        for i in range(k + 1, n + 1):
+                                l1 *= i
+                        for i in range(1, n - k + 1):
+                                l1 /= i
+                        return l1
+
+                x = 0
+                #f = NamedTemporaryFile(delete=True)
+                # hp - hp lost in battle
+                for hp in range(0, k):
+                        y = newt(hp + l, hp) * pow(p, hp) * pow(1 - p, l)
+                        #print(b"%d %.20f\n" % (defhp - attfp * hp, y))
+                        x += y
+
+                # defender lost
+                # al - attacker loss of hitpoints
+                y = 0
+                for al in range(0, l):
+                        y += newt(k - 1 + al, al) * pow(p, k - 1) * pow(1 - p, al) * p
+                x += y
+                #print(b"%d %.20f\n" % (0, y))
+                #f.flush()
+                #name = "%s/plots/warcalc/%s-%s-%s-%s-%s-%s.svg" % (settings.MEDIA_ROOT, atthp, defhp, attfp, deffp, attpo, defpo)
+                #system("gnuplot -e 'load \"%s/warcalc.p\"; set output \"%s\"; set ylabel \"probability\"; set xrange [0:%s]; plot \"%s\" with boxes linecolor rgb \"#000000\"' 2>/tmp/gnuplot_errors" % (settings.PLOT_PATH, name, atthp, f.name));
+                #f.close()
+                return y
+
+
+        if request['method'] == 'POST':
+                #form = WarCalcForm(request.POST)
+                #if form.is_valid():
+                        astr = request['astr']
+                        ahp = request['ahp']
+                        afp = request['afp']
+                        dstr = request['dstr']
+                        dhp = request['dhp']
+                        dfp = request['dfp']
+
+                        p = astr / (astr + dstr);
+                        def winprob(hp1, hp2, fp1, fp2):
+                                dp = [ [ 0.0 for i in range(hp2+1) ] for j in range(hp1+1) ]
+                                for i in range(hp1+1):
+                                        for j in range(hp2+1):
+                                                if i == 0:
+                                                        dp[i][j] = 0.0
+                                                        continue
+                                                if j == 0:
+                                                        dp[i][j] = 1.0
+                                                        continue
+                                                dp[i][j] = p * dp[max(0, i - fp2)][j] + (1.0 - p) * dp[i][max(0, j - fp1)]
+                                return 100.0 * dp[int(hp1)][int(hp2)]
+
+                        def exphp(lp, hp1, hp2, fp1, fp2):
+                                dh = [ [ 0.0 for i in range(hp2+1) ] for j in range(hp1+1) ]
+                                for i in range(hp1+1):
+                                        for j in range(hp2+1):
+                                                if i == 0:
+                                                        dh[i][j] = 0.0
+                                                        continue
+                                                if j == 0:
+                                                        dh[i][j] = i
+                                                        continue
+                                                dh[i][j] = lp * dh[max(0, i - fp2)][j] + (1.0 - lp) * dh[i][max(0, j - fp1)]
+                                return dh[int(hp1)][int(hp2)]
+
+                        tab = []
+                        ndhp = dhp
+                        for i in range(20):
+                                defprob = winprob(ndhp, ahp, dfp, afp)
+                                attprob = 100 - defprob
+                                dexphp = exphp(p, ndhp, ahp, dfp, afp)
+                                aexphp = exphp(1.0 - p, ahp, ndhp, afp, dfp)
+                                delta = ndhp - dexphp
+                                tab.append([attprob, aexphp, defprob, dexphp, delta])
+                                ndhp = int(dexphp)
+                                if ndhp == 0:
+                                        break
+
+                        prob = remhp(ahp, dhp, afp, dfp, astr, dstr)
+
+                        form = {
+                                'astr': astr,
+                                'ahp': ahp,
+                                'afp': afp,
+                                'dstr': dstr,
+                                'dhp': dhp,
+                                'dfp': dfp,
+                        }
+                        return {
+                                'form': form,
+                                'tab': tab,
+                                'prob': "%.6f" % (prob * 100),
+                        }
+        else:
+                form = {
+                        'astr': 12.0,
+                        'ahp': 20,
+                        'afp': 1,
+                        'dstr': 4.5,
+                        'dhp': 20,
+                        'dfp': 1,
+                }
+
+        return{'form': form}
+
+#print(warcalc({
+#        'method': 'POST',
+#        'astr': 4,
+#        'ahp': 10,
+#        'afp': 2,
+#        'dstr': 6,
+#        'dhp': 10,
+#        'dfp': 1,
+#}))
+
+for x in range(10, 17):
+        print(x)
---
+++ src/warcalc/warcalc.go
@@ -0,0 +1,153 @@
+package warcalc
+
+import (
+	"freecocoa/src/models"
+	"math"
+)
+
+func noNeg(x int) int {
+	if x >= 0 {
+		return x
+	}
+
+	return 0
+}
+
+func createRange(n1, n2 int) []int {
+	elems := n2 - n1
+
+	if elems <= 0 {
+		return []int{}
+	}
+
+	arr := make([]int, elems)
+
+	for i := 0; i < elems; i++ {
+		arr[i] = n1 + i
+	}
+
+	return arr
+}
+
+func newt(n, k int) int {
+	l1 := 1
+
+	for _, i := range createRange(k+1, n+1) {
+		l1 *= i
+	}
+
+	for _, i := range createRange(1, n-k+1) {
+		l1 /= i
+	}
+
+	return l1
+}
+
+func remhp(atthp, defhp, attfp, deffp int, attpo, defpo float64) float64 {
+	p := attpo / (attpo + defpo)
+	k := int(float64(defhp / attfp))
+	l := int(float64(atthp / deffp))
+
+	x := 0.0
+
+	for hp := 0; hp < k; hp++ {
+		y := float64(newt(hp+l, hp)) * math.Pow(p, float64(hp)) * math.Pow(1-p, float64(l))
+		x += y
+	}
+
+	y := 0.0
+
+	for al := 0; al < l; al++ {
+		y += float64(newt(k-1+al, al)) * math.Pow(p, float64(k-1)) * math.Pow(1-p, float64(al)) * p
+	}
+
+	x += y
+	_ = x
+
+	return y
+}
+
+func winprob(p float64, hp1, hp2, fp1, fp2 int) float64 {
+	dp := [][]float64{}
+
+	for i := 0; i < hp1; i++ {
+		for j := 0; j < hp2; j++ {
+			if i == 0 {
+				dp[i][j] = 0
+				continue
+			}
+
+			if j == 0 {
+				dp[i][j] = 1
+				continue
+			}
+
+			dp[i][j] = p*dp[noNeg(i-fp2)][j] + (1-p)*dp[i][noNeg(j-fp1)]
+		}
+	}
+
+	return 100 * dp[hp1][hp2]
+}
+
+func exphp(lp float64, hp1, hp2, fp1, fp2 int) float64 {
+	dh := [][]float64{}
+
+	for i := 0; i < hp1; i++ {
+		for j := 0; j < hp2; j++ {
+			if i == 0 {
+				dh[i][j] = 0
+				continue
+			}
+
+			if j == 0 {
+				dh[i][j] = float64(i)
+				continue
+			}
+
+			dh[i][j] = lp*dh[noNeg(i-fp2)][j] + (1-lp)*dh[i][noNeg(j-fp1)]
+		}
+	}
+
+	return dh[hp1][hp2]
+}
+
+func Warcalc(avd models.FinalStats) models.CombatResults {
+	astr := avd.Attacker.AP
+	ahp := avd.Attacker.HP
+	afp := avd.Attacker.FP
+	dstr := avd.Defender.DP
+	dhp := avd.Defender.HP
+	dfp := avd.Defender.FP
+	p := astr / (astr + dstr)
+	tab := make([]models.CombatResult, 0, 20)
+	ndhp := (dhp)
+
+	for i := 0; i < 20; i++ {
+		defprob := winprob(p, ndhp, ahp, dfp, afp)
+		attprob := 100 - defprob
+		dexphp := exphp(p, ndhp, ahp, dfp, afp)
+		aexphp := exphp(1.0-p, ahp, ndhp, afp, dfp)
+		delta := float64(ndhp) - dexphp
+		results := models.CombatResult{
+			AttProb:  attprob,
+			AttHP:    aexphp,
+			DefProb:  defprob,
+			DefHP:    dexphp,
+			DefDelta: delta,
+		}
+
+		tab = append(tab, results)
+
+		ndhp := int(dexphp)
+		if ndhp == 0 {
+			break
+		}
+	}
+
+	prob := remhp(ahp, dhp, afp, dfp, astr, dstr)
+
+	return models.CombatResults{
+		Prob:    prob,
+		Results: tab,
+	}
+}
